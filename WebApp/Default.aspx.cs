bfab8ae21542bd62b7caa782fe73d4f209d45410
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2016-11-06 23:08:10 -0200

    aula 51 - multicamadas

--- WebApp/Default.aspx.cs
+++ WebApp/Default.aspx.cs
@@ -5,18 +5,40 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Editora.DataAccess;
+using System.Data;
 
 namespace WebApp
 {
     public partial class Default : System.Web.UI.Page
     {
+        private DataSet getDataSet()
+        {
+            if (Cache["ds"] == null)
+            {
+                var ctx = new Repository();
+                Cache["ds"] = ctx.Select();
+            }
+            return Cache["ds"] as DataSet;
+        }
+        private DataView getDataView()
+        {
+            var ds = getDataSet();
+            var dt = ds.Tables[0];
+            var dv = new DataView(dt);
+            dv.Sort = "CAPA";
+            dv.RowFilter = "NUM_EDICAO > 103";
+            return dv;
+
+
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
-                var ctx = new Repository();
-                var ds = ctx.Select();
-                GridView1.DataSource = ds;
+                //var ctx = new Repository();
+                //var ds = ctx.Select();
+                GridView1.DataSource = getDataView();
                 GridView1.DataBind();
                 //dr.Close();
             }
